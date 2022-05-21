49c2f28843eb6efd00807fd7aa9dbccfab41486d
Paras Rawat <parasrawatjr@gmail.com>
2022-05-22 00:21:17 +0530

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Shivam Shekhar.
+      <time>{YEAR}</time> © Paras Rawat.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
